06f4840a1062eb24c0b9dc059ee7b5f9fab59b85
Marco Antonio <50679768+marcosantosjr@users.noreply.github.com>
2026-05-26 22:13:26 -0300

    Add files via upload

---
+++ jogo_da_velha.py
@@ -0,0 +1,60 @@
+"""
+Jogo da Velha no Python
+"""
+
+def mostrar_tabuleiro(tab):
+    for linha in tab:
+        print(" | ".join(linha))
+
+def verificar_vitoria(tab, jogador):
+    # Verifica as linhas
+    for i in range(3):
+        if tab[i][0] == jogador and tab[i][1] == jogador and tab[i][2] == jogador:
+            return True
+
+    # Verifica as colunas
+    for i in range(3):
+        if tab[0][i] == jogador and tab[1][i] == jogador and tab[2][i] == jogador:
+            return True
+
+    # Verifica diagonais
+    if tab[0][0] == jogador and tab[1][1] == jogador and tab[2][2] == jogador:
+        return True
+    if tab[2][0] == jogador and tab[1][1] == jogador and tab[0][2] == jogador:
+        return True
+
+    return False
+
+tabuleiro = [["1", "2", "3"],
+             ["4", "5", "6"],
+             ["7", "8", "9"]]
+
+jogador_atual = "X"
+
+rodada = 1
+
+while rodada <= 9:
+    mostrar_tabuleiro(tabuleiro)
+    escolha = input(f"Jogador {jogador_atual}, escolha uma posição (1-9): ")
+    pos = int(escolha) - 1
+    linha, coluna = pos // 3, pos % 3
+
+    if tabuleiro[linha][coluna] in ["X", "O"]:
+        print("Posição já ocupada. Tente outra.")
+        rodada -= 1
+
+    tabuleiro[linha][coluna] = jogador_atual
+
+    if verificar_vitoria(tabuleiro, jogador_atual):
+        mostrar_tabuleiro(tabuleiro)
+        print(f"Jogador {jogador_atual} venceu!")
+        break
+
+    if jogador_atual == "O":
+        jogador_atual = "X"
+    else:
+        jogador_atual = "O"
+
+    rodada += 1
+
+mostrar_tabuleiro(tabuleiro)
